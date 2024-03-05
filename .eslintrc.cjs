4379865020346cd6685046e631c145da584e408a
Ryan <rpbiwer@gmail.com>
2024-03-05 13:50:48 -0500

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.ConfigType} */
+module.exports = {
+  extends: [`plugin:deprecation/recommended`, `@ryb73`],
+
+  overrides: [
+    {
+      files: [`src/index.ts`],
+      rules: {
+        "@typescript-eslint/parameter-properties": `off`,
+        "import/no-unused-modules": `off`,
+      },
+    },
+  ],
+};
